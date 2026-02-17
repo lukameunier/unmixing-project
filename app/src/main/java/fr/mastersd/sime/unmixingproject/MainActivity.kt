5d718f3adf9bf5ccd56069613a5c2d5d84990ad7
Luka Meunier <luka.meunier@univ-rouen.fr>
2026-02-17 10:35:27 +0100

    feat(injection): add the android entry point and app file for future usage of the hilt and dagger

--- app/src/main/java/fr/mastersd/sime/unmixingproject/MainActivity.kt
+++ app/src/main/java/fr/mastersd/sime/unmixingproject/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import fr.mastersd.sime.unmixingproject.navigation.Navigation
 import fr.mastersd.sime.unmixingproject.ui.theme.UnmixingProjectTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/fr/mastersd/sime/unmixingproject/app/UnmixingApp.kt
@@ -0,0 +1,7 @@
+package fr.mastersd.sime.unmixingproject.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class UnmixingApp: Application()
